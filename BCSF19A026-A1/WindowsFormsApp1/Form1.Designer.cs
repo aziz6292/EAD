585ae9226ceaf05232c73ed7fb7add4c828b88fb
Abdul Aziz <maya6862422@gmail.com>
2023-10-15 20:46:07 +0500

    first commit

---
+++ BCSF19A026-A1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.first = new System.Windows.Forms.TextBox();
+            this.last = new System.Windows.Forms.TextBox();
+            this.imageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.subjects_list = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // first
+            // 
+            this.first.Location = new System.Drawing.Point(129, 73);
+            this.first.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.first.Name = "first";
+            this.first.Size = new System.Drawing.Size(241, 26);
+            this.first.TabIndex = 0;
+            this.first.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // last
+            // 
+            this.last.Location = new System.Drawing.Point(129, 171);
+            this.last.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.last.Name = "last";
+            this.last.Size = new System.Drawing.Size(241, 26);
+            this.last.TabIndex = 1;
+            // 
+            // imageList1
+            // 
+            this.imageList1.ColorDepth = System.Windows.Forms.ColorDepth.Depth8Bit;
+            this.imageList1.ImageSize = new System.Drawing.Size(16, 16);
+            this.imageList1.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "EAD",
+            "MAD",
+            "Numerical Computing",
+            "Quran",
+            "TBW"});
+            this.comboBox1.Location = new System.Drawing.Point(75, 246);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(295, 28);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(35, 250);
+            this.checkBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(22, 21);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(86, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "First Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(14, 177);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(86, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Last Name";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(14, 445);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(55, 20);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Result";
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Black;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button2.Location = new System.Drawing.Point(269, 322);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(84, 29);
+            this.button2.TabIndex = 9;
+            this.button2.Text = "Add";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // subjects_list
+            // 
+            this.subjects_list.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.subjects_list.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.subjects_list.FormattingEnabled = true;
+            this.subjects_list.ItemHeight = 25;
+            this.subjects_list.Location = new System.Drawing.Point(439, 72);
+            this.subjects_list.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.subjects_list.Name = "subjects_list";
+            this.subjects_list.Size = new System.Drawing.Size(449, 279);
+            this.subjects_list.TabIndex = 10;
+            this.subjects_list.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AllowDrop = true;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.ClientSize = new System.Drawing.Size(900, 562);
+            this.Controls.Add(this.subjects_list);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.last);
+            this.Controls.Add(this.first);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "Form1";
+            this.Text = "BCSF19A026_A1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox first;
+        private System.Windows.Forms.TextBox last;
+        private System.Windows.Forms.ImageList imageList1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ListBox subjects_list;
+    }
+}
+
---
+++ BCSF19A026-A1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml.Linq;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        List<string> subjects;
+        public Form1()
+        {
+            InitializeComponent();
+            this.subjects = new List<String>();
+        }
+
+
+
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            string firstname = this.first.Text;
+            string lastname = this.last.Text;
+            if (string.IsNullOrEmpty(firstname) || string.IsNullOrEmpty(lastname))
+            {
+                MessageBox.Show("All fields are required.", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            
+            string subject = this.comboBox1.SelectedItem != null ? this.comboBox1.SelectedItem.ToString() : "";
+            if (string.IsNullOrEmpty(subject))
+            {
+                MessageBox.Show("Please add atleast one subject.", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            
+            if (subjects.Contains(subject))
+            {
+                MessageBox.Show($"{subject} has already been added", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                return;
+            }
+            else
+            {
+                bool check = this.checkBox1.Checked;
+                if (check)
+                {
+                    subjects.Add(subject);
+                    subjects_list.Items.Add(subject);
+                    MessageBox.Show($"{subject} has been added", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                else
+                {
+                    MessageBox.Show("Only students with pre-requisites clear are allowed to enroll in a subject", "Alert", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+                
+            }
+            
+            if (subjects.Count > 0)
+            {
+                this.label4.Text = $"First Name: {firstname}, Last Name: {lastname}\n" +
+                    $"Subjects Enrolled: ";
+                foreach (var sub in this.subjects)
+                {
+                    this.label4.Text += sub;
+                    label4.Text += ", ";
+                }
+                checkBox1.Checked = false;
+            }
+           
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
